6e83c17698f38a934efffd82027aabd120f5da29
AdeilsonESilva <adeilson@gmail.com>
2020-02-25 17:35:26 -0300

    - Logica para abertura do campos e marcação

--- lib/screens/campo_minado_app.dart
+++ lib/screens/campo_minado_app.dart
@@ -1,20 +1,51 @@
 import 'package:campo_minado/components/resultado_widget.dart';
 import 'package:campo_minado/components/tabuleiro_widget.dart';
 import 'package:campo_minado/models/campo.dart';
+import 'package:campo_minado/models/explosao_exception.dart';
 import 'package:campo_minado/models/tabuleiro.dart';
 import 'package:flutter/material.dart';
 
-class CampoMinadoApp extends StatelessWidget {
+class CampoMinadoApp extends StatefulWidget {
+  @override
+  _CampoMinadoAppState createState() => _CampoMinadoAppState();
+}
+
+class _CampoMinadoAppState extends State<CampoMinadoApp> {
+  bool _venceu = null;
+  Tabuleiro _tabuleiro = Tabuleiro(
+    linhas: 12,
+    colunas: 12,
+    qtdeBombas: 3,
+  );
+
   _reiniciar() {
-    print('reiniciar...');
+    setState(() {
+      _venceu = null;
+      _tabuleiro.reiniciar();
+    });
   }
 
   _abrir(Campo campo) {
-    print('abriu...');
+    if (_venceu != null) return;
+
+    setState(() {
+      try {
+        campo.abrir();
+        if (_tabuleiro.resolvido) _venceu = true;
+      } on ExplosaoException {
+        _venceu = false;
+        _tabuleiro.revelarBombas();
+      }
+    });
   }
 
   _alternarMarcacao(Campo campo) {
-    print('alternar...');
+    if (_venceu != null) return;
+
+    setState(() {
+      campo.alternarMarcacao();
+      if (_tabuleiro.resolvido) _venceu = true;
+    });
   }
 
   @override
@@ -22,16 +53,12 @@ class CampoMinadoApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: ResultadoWidget(
-          venceu: false,
+          venceu: _venceu,
           onReiniciar: _reiniciar,
         ),
         body: Container(
           child: TabuleiroWidget(
-            tabuleiro: Tabuleiro(
-              linhas: 15,
-              colunas: 15,
-              qtdeBombas: 10,
-            ),
+            tabuleiro: _tabuleiro,
             onAbrir: _abrir,
             onAlternarMarcacao: _alternarMarcacao,
           ),
